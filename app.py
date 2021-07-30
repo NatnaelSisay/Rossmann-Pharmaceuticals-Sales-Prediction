6a3c842956cb1ae2fd799adfc313157fd7fac227
NatnaelSisay <natyjava8@gmail.com>
2021-07-30 20:03:56 +0300

    added deep learning and some features

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ import streamlit as st
 st.title('Stream Lit from Deployment')
 st.sidebar.write('Sidebar')
 
+
+# input taken from dashboard
+feature_columns = ['Store', 'DayOfWeek', 'Open', 'Promo',  'SchoolHoliday', 'Day', 'WeekOfYear','Month', 'Year', 'StoreType',
+              'Assortment','CompetitionDistance', 'Promo2']
+
 ## INPUTS
 value1 = st.sidebar.number_input('some value')
